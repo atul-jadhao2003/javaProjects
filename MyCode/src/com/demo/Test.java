e5e9d616960e142a8e54b853d0a0d6b7ff494afe
ashus <ashus@Ashu>
2025-07-16 00:40:06 +0530

    First Commit
    Uploading all files

---
+++ MyCode/src/com/demo/Test.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+
+		System.out.println("Addition = " + cal.add(25, 55));
+		System.out.println("Subtraction = " + cal.sub(50, 20));
+
+	}
+}
